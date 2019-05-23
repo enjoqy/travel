73dea08992a5d97ccff3ccf0ce456e1522297c28
hacker <gdlzhh321@163.com>
2019-05-23 21:18:56 +0800

    Initial commit

---
+++ src/main/java/cn/itcast/travel/web/servlet/ActiveUserServlet.java
@@ -0,0 +1,40 @@
+package cn.itcast.travel.web.servlet;
+
+import cn.itcast.travel.service.UserService;
+import cn.itcast.travel.service.impl.UserServiceImpl;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Author junhi
+ * @Date 2019/5/23 20:07
+ */
+@WebServlet("/activeUserServlet")
+public class ActiveUserServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+        // 1、获取激活码
+        String code = request.getParameter("code");
+        if(code != null){
+            //2、调用service完成激活
+            UserService service = new UserServiceImpl();
+            boolean flag = service.active(code);
+
+            // 3、判断标记
+            String msg = null;
+            if(flag){
+                //激活成功
+                msg = "激活成功，请<a href='login.html'>登录</a>";
+            }else{
+                //激活失败
+            }
+
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response){
+
+    }
+}
